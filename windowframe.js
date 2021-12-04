caf49d8a1d8e594013efb750e2bef659f22158bc
TechTOPO <tech@agencetopo.qc.ca>
2021-12-04 16:24:17 -0500

    sorry I'm late

--- windowframe.js
+++ windowframe.js
@@ -21,10 +21,8 @@ function windowframe(x,y,height, width) {
 
     this.currentid = id;
     this.windowHTML = function() {
-        var w,h;
         var a = document.createElement('div');
-        
-        a.setAttribute('class','windowframe');
+        a.setAttribute('class','windowframe draggable');
         a.setAttribute('style','left:'+x+';top:'+y+';z-index:'+z+';width:'+height+'vw;height:'+width+'vh;');
 
         return a;
@@ -43,19 +41,28 @@ function windowframe(x,y,height, width) {
 
 
   /*version randomize*/
-  function addwindowframe(content){
-    x = 0+(Math.floor(Math.random() * 40)) +"vw";
-    y = 0+(Math.floor(Math.random() * 20)) +"vh";
-    w = 40+(Math.floor(Math.random() * 40)) +"";
-    h = 40+(Math.floor(Math.random() * 40)) +"";
+  function addwindowframe(content, title){
+    let x = 0+(Math.floor(Math.random() * 40)) +"vw";
+    let y = 0+(Math.floor(Math.random() * 20)) +"vh";
+    let w = 40+(Math.floor(Math.random() * 40)) +"";
+    let h = 40+(Math.floor(Math.random() * 40)) +"";
 
 
     var newframe = new windowframe (x,y,h,w);
     var b = newframe.windowHTML();
+
     document.body.appendChild(b);
     b.id = "window"+newframe.currentid;
-    b.innerHTML = content;
+    b.innerHTML += '<div class="wholeframe"><div id="topframe'+newframe.currentid+'"; class="topframe">'+title+'</div>';
+    b.innerHTML += '<iframe src='+content+'></iframe><div>';
+    b.addEventListener("mousedown",function(){
+      const cID = document.getElementById("window"+newframe.currentid);
+      
+      cID.style.zIndex = z;
+      z++;
+    })
   id++;
   z++;
 }
+
 
