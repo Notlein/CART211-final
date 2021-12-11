bf29b6fc7a2898144005e7f95e68ed3c7458b248
TechTOPO <tech@agencetopo.qc.ca>
2021-12-10 21:08:06 -0500

    bloup

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+// SCRIPTS
+// 
+
+// Loading screen (it'S an mp4 with a fadeOut)
+
 setTimeout(function() {
   $("#start-screen").fadeOut();
 }, 3000)
 
+
+// function for toggling Font Awesome icons
 function toggleIcon(id, activeClasses, inactiveClasses) {
   var id, activeClasses, inactiveClasses;
   const icon = document.getElementById(id);
@@ -13,11 +20,14 @@ function toggleIcon(id, activeClasses, inactiveClasses) {
   }
 }
 
+// close function for the start menu
 function closeStart() {
   const menustyle = document.getElementById("start-box").style;
   menustyle.display = "none";
 }
 
+
+// Toggle function for the start menu
 function openStart() {
   const menustyle = document.getElementById("start-box").style;
   if (menustyle.display !== "none") {
@@ -27,6 +37,7 @@ function openStart() {
   }
 }
 
+// This function calls the lock screen, locking the user of the computer foreverrrr
 function logOff() {
   const logoffstyle = document.getElementById("log-off").style;
   if (logoffstyle.opacity !== "1") {
@@ -34,34 +45,51 @@ function logOff() {
       logoffstyle.opacity = '1';
   }
 }
+
+
 var cmptsml = 0;
 
 function oops() {
+
+  // construct array of icons, all the same except two
   let newicon = new Array('<i class="far fa-dizzy"></i>',
       '<i class="far fa-dizzy"></i>', '<i class="far fa-dizzy"></i>',
       '<i class="far fa-dizzy"></i>', '<i class="far fa-dizzy"></i>',
       '<i class="far fa-dizzy"></i>', '<i class="far fa-grin-tears"></i>',
       '<i class="fas fa-virus"></i>');
+
   const logoff = document.getElementById('log-off-text');
   const smile = document.getElementById("smiley");
+
   logoff.style.display = 'block';
+
+  // MAkes sure the first icon is always the regular icon 'fa-dizzy'
   if (cmptsml === 0) {
       smile.innerHTML = '<i class="far fa-dizzy"></i>';
   } else {
       smile.innerHTML = newicon[Math.floor(Math.random() * newicon.length)];
   }
+
   cmptsml++;
 }
 
+
+// Function to 'shut down' the computer/website
+
 function powerOff() {
+
   $("#start-box").remove();
   $("#bottom-bar").remove();
+
+  // initialization
   document.body.style.backgroundColor = "black";
   document.body.style.position = "absolute";
   document.body.style.display = "flex";
   document.body.style.alignContent = "center";
   document.body.style.justifyContent = "center";
   document.body.style.alignItems = "center";
+
+  // animation
   $("main").animate({
       left: "50%",
       top: "50%",
@@ -70,15 +98,13 @@ function powerOff() {
       opacity: 0.35
   },'fast').animate({opacity:0},4000);
   
+  // remove the inside of the body 5s after the animation
   setTimeout(function removeMain(){$("main").remove();$("div").remove();} , 5000 )
      
 
-  
- 
-
-
 }
 
+// 
 function changeHTML(id, content){
   $(id).html(content);
 }
